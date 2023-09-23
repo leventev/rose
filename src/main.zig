43ba7127e52e77097a16ca18b136e1633d5f2788
leventev <levente.vamos@protonmail.com>
2023-09-23 20:02:02 +0200

    dont allocate memory for line reading

--- src/main.zig
+++ src/main.zig
@@ -8,6 +8,7 @@ comptime {
 }
 
 const PROMPT: []const u8 = "$ ";
+const MAX_LINE_LEN = 4096;
 
 pub fn print_prompt() !void {
     _ = try rook.io.out.writeAll(PROMPT);
@@ -15,18 +16,15 @@ pub fn print_prompt() !void {
 
 pub fn main(args: []const [*:0]const u8, envp: [*:null]const ?[*:0]const u8) !void {
     try env.init_env(envp);
-    _ = env;
     _ = args;
 
-    var line_buff = std.ArrayList(u8).init(rook.page_allocator);
-    defer line_buff.deinit();
-    var line_buff_writer = line_buff.writer();
+    var line_buff: [MAX_LINE_LEN]u8 = undefined;
+    var buff_stream = std.io.fixedBufferStream(&line_buff);
 
     const stdin_reader = rook.io.in.reader();
-    while (true) : (line_buff.clearRetainingCapacity()) {
+    while (true) : (buff_stream.reset()) {
         try print_prompt();
-        try stdin_reader.streamUntilDelimiter(line_buff_writer, '\n', null);
-
-        try command.handle_line(rook.page_allocator, line_buff.items);
+        try stdin_reader.streamUntilDelimiter(buff_stream.writer(), '\n', MAX_LINE_LEN);
+        try command.handle_line(rook.page_allocator, buff_stream.getWritten());
     }
 }
